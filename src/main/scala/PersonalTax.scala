c2ed5ef32cee0648c08a6ab7b2ac6fbd59fce263
Suo Qin <kpsesuo@gmail.com>
2015-12-25 23:23:51 +0800

    move incomeTax logic out of personal tax object

--- src/main/scala/PersonalTax.scala
+++ src/main/scala/PersonalTax.scala
@@ -1,27 +1,15 @@
-case class PersonalTax(salary: Double, superRate: Double) {
+case class PersonalTax(salary: Double, superRate: Double, incomeTaxRate: TaxRate) {
   implicit def doubleToRoundingDollar(number: Double): RoundingDollar = RoundingDollar(number)
+
   implicit def IntToRoundingDollar(number: Int): RoundingDollar = RoundingDollar(number)
 
-  def superTax: RoundingDollar = {
-    grossIncome * superRate * 0.01
-  }
+  def superTax: RoundingDollar = grossIncome * superRate * 0.01
 
-  def netIncome: RoundingDollar = {
-    grossIncome - incomeTax
-  }
+  def netIncome: RoundingDollar = grossIncome - incomeTax
 
-  def incomeTax: RoundingDollar = {
-    if (salary > 80000) {
-      (17547 + (salary - 80000) * 0.37) / 12
-    } else {
-      (3572 + (salary - 37000) * 0.325) / 12
-    }
+  def incomeTax: RoundingDollar = incomeTaxRate.of(salary)
 
-  }
+  def grossIncome: RoundingDollar = salary / 12
 
-  def grossIncome: RoundingDollar = {
-    salary / 12
-  }
-
-  def report = List(grossIncome,incomeTax,netIncome,superTax).mkString(",")
+  def report = List(grossIncome, incomeTax, netIncome, superTax).mkString(",")
 }
