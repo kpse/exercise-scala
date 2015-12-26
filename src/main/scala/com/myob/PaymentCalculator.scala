d2e154df8a6957ee18f20f43030ec4ebd70624ea
Suo Qin <kpsesuo@gmail.com>
2015-12-26 12:16:58 +0800

    add error scenarios for employee info input

--- src/main/scala/com/myob/PaymentCalculator.scala
+++ src/main/scala/com/myob/PaymentCalculator.scala
@@ -1,12 +1,13 @@
 package com.myob
 
 case class PaymentCalculator(tax: IncomeTax) {
-  val employee = """^(\w+),(\w+),(\d+),(\d+)%,(.+)$""".r
+  val employee = """^(\w+)\s*,\s*(\w+)\s*,\s*(\d+)\s*,\s*(\d+)%\s*,\s*(.+)\s*$""".r
 
   def payslip(input: String) = input match {
     case employee(firstName, lastName, salary, superRate, period) =>
       PaymentDetail(s"$firstName $lastName", period, PersonalTax(salary.toDouble, superRate.toDouble, tax)).report()
-    case _ => ""
+    case _ =>
+      "The employee information consists of first name, last name, annual salary, super rate (%) and payment period, separated by comma."
   }
 
 
--- src/test/scala/com/myob/PaymentCalculatorSpec.scala
+++ src/test/scala/com/myob/PaymentCalculatorSpec.scala
@@ -8,8 +8,10 @@ class PaymentCalculatorSpec extends Specification {
   This is a specification for the 'Payment Calculator'
 
   The Payment Calculator should
-    report payslip for David                                $payslipForDavid
+    report payslip for David                               $payslipForDavid
     report payslip for Ryan                                $payslipForRyan
+    report error for wrong input                           $reportError
+    ignore spaces around comma                             $handleMoreSpace
                                                       """
 
   def payslipForDavid = {
@@ -24,6 +26,20 @@ class PaymentCalculatorSpec extends Specification {
     val taxTable = new IncomeTax(List("$80,001 - $180,000      $17,547 plus 37c for each $1 over $80,000"))
 
     val payslip = PaymentCalculator(taxTable).payslip("Ryan,Chen,120000,10%,01 March – 31 March")
+
+    payslip must equalTo("Ryan Chen,01 March – 31 March,10000,2696,7304,1000")
+  }
+
+  def reportError = {
+    val payslip = PaymentCalculator(new IncomeTax(List())).payslip("Something I don't understand")
+
+    payslip must equalTo("The employee information consists of first name, last name, annual salary, super rate (%) and payment period, separated by comma.")
+  }
+
+  def handleMoreSpace = {
+    val taxTable = new IncomeTax(List("$80,001 - $180,000      $17,547 plus 37c for each $1 over $80,000"))
+
+    val payslip = PaymentCalculator(taxTable).payslip("Ryan ,Chen,120000,10%,01 March – 31 March")
 
     payslip must equalTo("Ryan Chen,01 March – 31 March,10000,2696,7304,1000")
   }
