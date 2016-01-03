da0fca3b00022d11b7a62ac00000bfeccdd03e7f
Suo Qin <kpsesuo@gmail.com>
2016-01-03 13:25:18 +0800

    format code

--- src/main/scala/com/myob/PersonalTax.scala
+++ src/main/scala/com/myob/PersonalTax.scala
@@ -14,11 +14,16 @@ case class PersonalTax(salary: Double, superRate: Double, incomeTaxRate: IncomeT
   def grossIncome: RoundingDollar = salary / 12
 
   def report = {
-    val result = for (income <- incomeTax; net <- netIncome; sup <- superTax)
-      yield List(grossIncome, income, net, sup).map(_.toInt).mkString(",")
+    val result = for {tax <- incomeTax
+                      net <- netIncome
+                      sup <- superTax
+    } yield List(grossIncome, tax, net, sup).map(_.toInt).mkString(",")
     result.getOrElse(s"Error in calculating tax: $errorMessage")
   }
 
-  def errorMessage = List(incomeTax, superTax).zip(List("No match range in Tax Table", "Invalid Super Tax Rate(0 ~ 50% inclusive)"))
-    .find(_._1.isEmpty).map(_._2).getOrElse("General Error.")
+  private def errorMessage = List(incomeTax, superTax)
+    .zip(List("No match range in Tax Table", "Invalid Super Tax Rate(0 ~ 50% inclusive)"))
+    .find(_._1.isEmpty)
+    .map(_._2)
+    .getOrElse("General Error.")
 }
