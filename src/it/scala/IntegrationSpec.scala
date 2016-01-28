59ff51bd4d6998695eb83b7ce3b5f557188249b4
Suo Qin <kpsesuo@gmail.com>
2016-01-28 20:16:47 +0800

    add integration test to support new format of tax table

--- src/it/scala/IntegrationSpec.scala
+++ src/it/scala/IntegrationSpec.scala
@@ -15,6 +15,7 @@ class IntegrationSpec extends Specification with Before {
     read user from file                              $readUserFromFile
     read tax table from file                         $readTaxTableFromFile
     write output to file                             $writeToFile
+    support new format of tax table                  $newFormat
                                                      """
 
   private val outputFilePath = "src/it/resources/output.txt"
@@ -33,6 +34,14 @@ class IntegrationSpec extends Specification with Before {
       MainApp.main(Array("src/it/resources/input.csv", "src/it/resources/zero_tax_table"))
     }
     outContent.toString.split("\n").toList must containAllOf(List("David Rudd,01 March – 31 March,5004,0,5004,450", "Ryan Chen,01 March – 31 March,10000,0,10000,950"))
+  }
+
+  def newFormat = {
+    val outContent = new java.io.ByteArrayOutputStream()
+    Console.withOut(outContent) {
+      MainApp.main(Array("src/it/resources/input.csv", "src/main/resources/new_tax_format_table"))
+    }
+    outContent.toString.split("\n").toList must containAllOf(List("David Rudd,01 March – 31 March,5004,334,4670,450", "Ryan Chen,01 March – 31 March,10000,1500,8500,950"))
   }
 
   def writeToFile = {
